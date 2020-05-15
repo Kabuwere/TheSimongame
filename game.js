91b534753a9f0dd30fe8ae73efc27864d462af38
Susan kabuwere <kabweresue@gmail.com>
2020-05-15 11:50:05 +0300

    complete step 2

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
 alert("working");
+
+//creating an array
+buttonColors = ["red", "blue", "green", "yellow"];
+gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.random();
+  randomNumber = randomNumber * 3;
+  randomNumber = Math.floor(randomNumber) + 1;
+  var randomChoosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChoosenColor);
+};
