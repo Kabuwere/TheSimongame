0f1deee5ef16fd3efaf44ef6ec80de1a51ee3727
Susan kabuwere <kabweresue@gmail.com>
2020-05-16 02:37:24 +0300

    complete step 3

--- game.js
+++ game.js
@@ -11,3 +11,26 @@ function nextSequence() {
   var randomChoosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChoosenColor);
 };
+
+
+//adding event listeners
+$("#blue").click(playSounds);
+$("#yellow").click(playSounds);
+$("#red").click(playSounds);
+$("#green").click(playSounds);
+
+//adding sounds
+function playSounds() {
+  var blue = new Audio("sounds/blue.mp3");
+  blue.play();
+
+  var green = new Audio("sounds/green.mp3");
+  green.play();
+
+  var yellow = new Audio("sounds/yellow.mp3");
+  yellow.play();
+
+  var red = new Audio("sounds/red.mp3");
+  red.play();
+
+}
